7b88cb42f75f293a122528d6bcaca65916eef533
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-11 15:14:19 +0000

    Display addition area to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ document.addEventListener("DOMContentLoaded", function(){
             else
             {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);                
+                runGame(gameType);              
             }
         })
     }
+
+    runGame("addition");
 
 
 } )
@@ -28,6 +30,16 @@ function runGame()
 {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition")
+    {
+        displayAdditionQuestion(num1,num2);
+    }
+    else
+    {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType} Aborting!`;
+    }
 }
 
 function checkAnswer()
@@ -50,8 +62,11 @@ function incrementWrongAnswer()
 
 }
 
-function displayAdditionQuestion()
+function displayAdditionQuestion(operand1,operand2)
 {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
