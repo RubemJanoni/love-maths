58db9bd13a9bfad54e18d9d4475c84054f0ae463
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-13 04:21:03 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,10 +55,12 @@ function checkAnswer()
     if(isCorrect)
     {
         alert("Hey! You got it right!");
+        incrementScore();
     }
     else
     {
         alert(`Ahhh... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}! `);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -86,13 +88,19 @@ function calculateCorrectAnswer()
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
 function incrementScore()
 {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer()
 {
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;   
 
 }
 
