908da34c3a3aa10ecf21abfd13801279acc1b641
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-14 04:17:12 +0000

    Add favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,14 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if(event.key === "Enter")
+        {
+            checkAnswer();
+        }
+    })
+    
+
     runGame("addition");
 
 
