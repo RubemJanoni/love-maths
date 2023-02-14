e73e2448f3e488fa9a53ff3b6554377c9c6c1160
Rubem Janoni <rubemjanoni@gmail.com>
2023-02-14 03:04:20 +0000

    Add multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,11 +35,16 @@ function runGame(gameType)
     {
         displayAdditionQuestion(num1, num2);
     }
+    else if(gameType === "multiply")
+    {
+        displayMultiplyQuestion(num1, num2);
+    }
     else
     {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType} Aborting!`;
     }
+
 }
 
 /**
@@ -81,6 +86,12 @@ function calculateCorrectAnswer()
     {
         return [operand1 + operand2, "addition"];
     }
+    else if(operator === "x")
+    {
+        return [operand1 * operand2, "multiply"];
+
+    }
+    
     else
     {
         alert(`Unimplemented operator ${operator}`);
@@ -117,9 +128,11 @@ function displaySubtractQuestion()
 
 }
 
-function displayMultiplyQuestion()
+function displayMultiplyQuestion(operand1, operand2)
 {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 
